21372a98ad011bdb1cfaf4ddf992b632c362703b
Jeremy Wood <js.wood206@gmail.com>
2020-09-14 17:10:23 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  
 end
