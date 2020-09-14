7f539d0bc011af7aec12ff6169f828480586c148
Jeremy Wood <js.wood206@gmail.com>
2020-09-14 17:04:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,14 @@
 class Owner
 
-  attr_reader :name
+  attr_reader :name, :species
 
   def initialize(name)
     @name = name
+    @species = "human"
+  end
+
+  def say_species
+    @species
   end
 
 
