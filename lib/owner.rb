ebd316821d071417f47fa504ae4f68e579add457
Jeremy Wood <js.wood206@gmail.com>
2020-09-14 17:05:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,14 +2,20 @@ class Owner
 
   attr_reader :name, :species
 
+  @@all = []
+  
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end
 
   def say_species
     @species
   end
 
+  def all
+    @@all
+  end
 
 end
